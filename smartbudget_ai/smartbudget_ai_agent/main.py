18a6ff88a154f7a154ba04e714d5abcf7f8a5ca7
Mamadou Sanogo <155233565+sanogomamadou@users.noreply.github.com>
2025-05-12 03:12:16 +0100

    Update main.py

--- smartbudget_ai/smartbudget_ai_agent/main.py
+++ smartbudget_ai/smartbudget_ai_agent/main.py
@@ -5,6 +5,8 @@ from agent.langchain_agent import run_agent
 from pydantic import BaseModel
 import uvicorn
 import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from agent.tools import verifierDepassementBudget
 
 app = FastAPI()
 
@@ -21,11 +23,27 @@ class AgentRequest(BaseModel):
     user_id: int
     query: str
 
-@app.post("/ask-agent")
+@app.post("/ask-agent")a
 async def ask_agent(data: AgentRequest):
     response = run_agent(data.query, data.user_id)
     print(f"Utilisateur ID au niveau de main : {data.user_id}")
     return {"response": response}
+
+# === FONCTION DE VÉRIF PÉRIODIQUE ===
+def check_budget_job():
+    print("Exécution automatique de l'agent de vérification des budgets...")
+    result = verifierDepassementBudget("start")
+    print(result)
+
+# === PLANIFICATION AUTOMATIQUE ===
+scheduler = BackgroundScheduler()
+scheduler.add_job(check_budget_job, 'interval', minutes=5)
+scheduler.start()
+
+# === ARRÊT DU SCHEDULER PROPREMENT ===
+@app.on_event("shutdown")
+def shutdown_event():
+    scheduler.shutdown()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
